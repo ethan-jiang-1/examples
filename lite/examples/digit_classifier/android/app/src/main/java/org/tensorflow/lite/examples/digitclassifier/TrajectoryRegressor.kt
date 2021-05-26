53e4626c20277a4504d77fdb563f2106567fda79
ethanmac <ethanmac@mac.com>
2021-05-26 17:23:11 +0800

    F: slower then D,  fixed10 is slower in mode F than mode D, span100: 520ms

--- lite/examples/digit_classifier/android/app/src/main/java/org/tensorflow/lite/examples/digitclassifier/TrajectoryRegressor.kt
+++ lite/examples/digit_classifier/android/app/src/main/java/org/tensorflow/lite/examples/digitclassifier/TrajectoryRegressor.kt
@@ -29,7 +29,7 @@ class TrajectoryRegressor(private val context: Context) {
   private var pumper: PumpMgr? = null
   private var mmsj: MwModelSgnJson? = null
 
-  private var selected_mode = "D"  //"P", "F", "D",  "I"
+  private var selected_mode = "F"  //"P", "F", "D",  "I"
   private var selected_options_str = ""
 
   private var fixed_batchsize = -1
@@ -166,8 +166,8 @@ class TrajectoryRegressor(private val context: Context) {
       //iocs = "NNAPI/T4/BHO"
       iocs = "XNNPACK/T4/BHO"
     } else if (model_filename.contains("_F.tflite")) {
-      //iocs = "/NNAPI/T4/BHO"
-      iocs = "/XNNPACK/T4/BHO"
+      iocs = "/NNAPI/T4/BHO"
+      //iocs = "/XNNPACK/T4/BHO"
     } else if (model_filename.contains("_D.tflite")) {
       iocs = "/NNAPI/T4/BHO"
       //iocs = "/XNNPACK/T4/BHO"
